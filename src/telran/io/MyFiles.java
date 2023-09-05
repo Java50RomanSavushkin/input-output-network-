30a66d94f65a4846f4bb3cbae443f377dd90261d
RomanSavushkin <romarsa82@gmail.com>
2023-09-06 00:43:36 +0300

    start

--- src/telran/io/MyFiles.java
+++ src/telran/io/MyFiles.java
@@ -5,22 +5,22 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class MyFiles {
-	
-public static void displayDir(String path, int maxDepth) throws IOException {
-	//TODO
-	//Throwing IllegalArgumentException in the case the given is not a directory
-	//Printing out the directory content using appropriate offsets showing which node belong to which directory
-	//printing type of node, for example 
-	//                              input-output-network - dir
-	//                               src - dir
-	//                                 telran - dir
-	//                                   io - dir
-	//                                    test - dir
-	//                                      FileSystemTests.java - file
-	try {
-		Files.walk(Path.of(path).toAbsolutePath().normalize(), maxDepth).forEach(p -> System.out.println(p.getFileName()));
-	} catch (IOException e) {
-		
+	public static void displayDir(String path, int maxDepth) throws IOException {
+		// TODO
+		// Throwing IllegalArgumentException in the case the given is not a directory
+		// Printing out the directory content using appropriate offsets showing which
+		// node belong to which directory
+		// printing type of node, for example
+		// input-output-network - dir
+		// src - dir
+		// telran - dir
+		// io - dir
+		// test - dir
+		// FileSystemTests.java - file
+		try {
+			Files.walk(Path.of(path).toAbsolutePath().normalize(), maxDepth)
+					.forEach(p -> System.out.println(p.getFileName()));
+		} catch (IOException e) {
+		}
 	}
-}
 }
--- src/telran/io/test/FileSystemTests.java
+++ src/telran/io/test/FileSystemTests.java
@@ -1,9 +1,11 @@
 package telran.io.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -12,7 +14,6 @@ import org.junit.jupiter.api.Test;
 import telran.io.MyFiles;
 
 class FileSystemTests {
-
 	@BeforeEach
 	void setUp() throws Exception {
 	}
@@ -28,23 +29,19 @@ class FileSystemTests {
 		assertTrue(current.isAbsolute());
 		System.out.printf("current.getFileName() -> %s\n", current.getFileName());
 		current = current.normalize();
-		
 		System.out.printf("current.toRealPath() -> %s\n", current.toRealPath());
 		System.out.printf("current.getNameCount() -> %s\n", current.getNameCount());
 		System.out.printf("current.getName(5) -> %s\n", current.getName(5));
 		System.out.printf("current.getFileName() -> %s\n", current.getFileName());
-		//Files.list(current).map(p -> p.getFileName()).forEach(p -> System.out.println(p));
-		//Files.walk(current).map(p -> p.getFileName()).forEach(p -> System.out.println(p));
+		// Files.list(current).map(p -> p.getFileName()).forEach(p ->
+		// System.out.println(p));
+		// Files.walk(current).map(p -> p.getFileName()).forEach(p ->
+		// System.out.println(p));
 		Files.walk(current, 2).map(p -> p.getFileName()).forEach(p -> System.out.println(p));
-		
-		
-		
-		
 	}
+
 	@Test
 	void displayDirTest() throws IOException {
 		MyFiles.displayDir("/Users/User", 3);
 	}
-	
-
 }
